4a426d05d284dfe4723c3a8c376d05eb5982a1aa
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-10-23 19:11:17 +0500

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,5 @@
 RPC = "https://eth-sepolia.g.alchemy.com/v2/tKHLQ21rwL-KSqVQMYeiLrIpEnlNL7_I"
 EXPLORER_URL = "https://sepolia.etherscan.io/tx/"
+
+UNICHAIN_RPC = "https://sepolia.unichain.org"
+UNICHAIN_EXPLORER_URL = "https://unichain-sepolia.blockscout.com/tx/"
